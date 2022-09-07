2bd28446d242afce150e5bd81118ac3668c1a07e
EmilWallett <nhlpool27@gmail.com>
2022-09-07 11:48:35 +0200

    Add project files.

---
+++ Kapitel 2 Uppgift 2.10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            Console.WriteLine("Min favorit bok är Percy Jackson.");
+            
+            
+            
+        }
+    }
+}
